9c19c84b2d675ab8a15f74971c37d852bf0da27c
tpSpace <ITCSIU21081@student.hcmiu.edu.vn>
2023-03-13 16:32:42 +0700

    Add register page &add shadow

--- Frontend/fontend-pdm/src/pages/register/Register.js
+++ Frontend/fontend-pdm/src/pages/register/Register.js
@@ -1,10 +1,38 @@
 import React from 'react';
-
+import './register.css';
 const Register = () => {
     return (
-        <div>
-            <h1>Register</h1>
-        </div>
+        <>
+            <div class="container">
+                <div class="register">
+                    <h1>Register</h1>
+                    <form action="">
+                        <label>
+                            Email
+                            <br/>
+                            <input type="email" placeholder="Email"></input>
+                            <br/>
+                        </label>
+                        
+                        <label>
+                            <span>Password</span>
+                            <br/>   
+                            <input type="text" placeholder="Password"></input>
+                            <br/>
+                        </label>
+
+                        <label>
+                            <span>Re-type password</span>
+                            <br/>
+                            <input type="text" placeholder="Re-type password"></input>
+                            <br/>
+                        </label>
+                        
+                        <button type="submit">SUBMIT</button>
+                    </form>
+                </div>
+            </div>
+        </>
     );
 };
 
