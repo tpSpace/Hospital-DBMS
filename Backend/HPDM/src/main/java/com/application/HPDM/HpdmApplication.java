53af26ebb21d17933384afb92b3d3110300bd67b
Ho-Gia-Khang <hogiakhanglatoi@gmail.com>
2023-04-13 13:51:48 +0700

    added spring boot framework

---
+++ Backend/HPDM/src/main/java/com/application/HPDM/HpdmApplication.java
@@ -0,0 +1,13 @@
+package com.application.HPDM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HpdmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HpdmApplication.class, args);
+	}
+
+}
---
+++ Backend/HPDM/src/test/java/com/application/HPDM/HpdmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.HPDM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HpdmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
