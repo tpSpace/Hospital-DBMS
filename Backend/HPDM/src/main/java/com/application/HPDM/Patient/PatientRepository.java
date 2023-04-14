7f6c4f0188d4cb4cffe43f0a95f8569a407d38da
tpSpace <ITCSIU21081@student.hcmiu.edu.vn>
2023-04-14 09:44:53 +0700

    Create Patient entity

---
+++ Backend/HPDM/src/main/java/com/application/HPDM/Patient/PatientRepository.java
@@ -0,0 +1,7 @@
+package com.application.HPDM.Patient;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PatientRepository extends JpaRepository<Patient, Long> {
+
+}
