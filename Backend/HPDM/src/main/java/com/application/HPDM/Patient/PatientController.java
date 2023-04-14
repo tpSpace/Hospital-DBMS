29ddfa4b3b5f37c992694861aacb8e6047b097f0
tpSpace <ITCSIU21081@student.hcmiu.edu.vn>
2023-04-14 14:43:48 +0700

    Add patient auto create db

--- Backend/HPDM/src/main/java/com/application/HPDM/Patient/PatientController.java
+++ Backend/HPDM/src/main/java/com/application/HPDM/Patient/PatientController.java
@@ -18,4 +18,5 @@ public class PatientController {
         return patientRepository.findAll();
     }
 
+
 }
--- Backend/HPDM/src/main/java/com/application/HPDM/Patient/PatientRepository.java
+++ Backend/HPDM/src/main/java/com/application/HPDM/Patient/PatientRepository.java
@@ -2,6 +2,7 @@ package com.application.HPDM.Patient;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface PatientRepository extends JpaRepository<Patient, Long> {
 
 }
