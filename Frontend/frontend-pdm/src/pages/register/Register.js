578133320cab490a2456a5e9cc56e2a213eedf36
tpSpace <ITCSIU21081@student.hcmiu.edu.vn>
2023-04-16 17:11:41 +0700

    Fixed scrolling in register & added more inputs & rename method in patientController

--- Frontend/frontend-pdm/src/pages/register/Register.js
+++ Frontend/frontend-pdm/src/pages/register/Register.js
@@ -1,7 +1,27 @@
-import React from 'react';
-import './register.css';
+import React, {useState} from 'react'
+import './Register.css'
+import axios from 'axios';
+import { Link,useNavigate } from 'react-router-dom';
 
 const Register = () => {
+
+    const [user,setUser] = useState({
+        patientFirstName: "",
+        patientLastName: "",
+        patientDob: "",
+        patientGender : "",
+        patientMedicalRecord: "N/A",
+        patientPhone: "",
+        patientEmail: "",
+        patientPassword: "",
+    });
+
+    const {patientFirstName,patientLastName,patientMedicalRecord,patientPhone,patientEmail,patientPassword} = user;
+
+    const onInputChange = (e) => {
+        setUser({...user,[e.target.name]:e.target.value});
+    }
+
     return (
         <>
             <div class="Container">
@@ -10,27 +30,75 @@ const Register = () => {
                     <h1>Register</h1>
                     <form id="register-form"> 
                     
-                        <label for="first-name">First name</label><br/>
-                        <input type="text" id="first-name" name="first-name" placeholder="Enter your first name" required/><br/>
+                        <label for="patientFirstName">First name</label><br/>
+                        <input 
+                            type={"text"}
+                            id="patientFirstName" 
+                            name="patientFirstName" 
+                            placeholder="Enter your first name" 
+                            
+                            value={patientFirstName}
+                            onChange={(e) => onInputChange(e)}
+                            required
+                        /><br/>
 
-                        <label for="last-name">Last name</label><br/>
-                        <input type="text" id="last-name" name="last-name" placeholder="Enter your last name" required/><br/>
+                        <label for="patientLastName">Last name</label><br/>
+                        <input 
+                            type="text" 
+                            id="patientLastName" 
+                            name="patientLastName" 
+                            placeholder="Enter your last name" 
+                            value={patientLastName}
+                            onChange={(e) => onInputChange(e)}
+                            required
+                        /><br/>
 
                         <label for="phone">Phone</label><br/>
-                        <input type="tel" id="phone" name="phone" placeholder="Enter your phone" required/><br/>
+                        <input 
+                            type="tel" 
+                            id="patientPhone" 
+                            name="patientPhone" 
+                            placeholder="Enter your phone" 
+                            value={patientPhone}
+                            onChange={(e) => onInputChange(e)}
+                            required
+                        /><br/>
 
-                        <label for="email">Email</label><br/>
-                        <input type="email" id="email" name="email" placeholder="Enter your email" required/><br/>            
+                        <label for="patientEmail">Email</label><br/>
+                        <input 
+                            type="email" 
+                            id="patientEmail" 
+                            name="patientEmail" 
+                            placeholder="Enter your email" 
+                            value={patientEmail}
+                            onChange={(e) => onInputChange(e)}
+                            required
+                        /><br/>            
 
-                        <label for="password">Password</label><br/>
-                        <input type="password" id="password" name="password" placeholder="Enter your password" required/><br/> 
+                        <label for="patientPassword">Password</label><br/>
+                        <input 
+                            type="password" 
+                            id="patientPassword" 
+                            name="patientPassword" 
+                            placeholder="Enter your password" 
+                            value={patientPassword}
+                            onChange={(e) => onInputChange(e)}
+                            required
+                        /><br/> 
 
-                        <label for="password2">Confirm Password</label><br/>
-                        <input type="password" id="password2" name="password2" placeholder="Confirm your password" required/><br/>
+                        <label for="patientMedicalRecord">Medical record</label><br/>
+                        <textarea  
+                            type="text" 
+                            id="patientMedicalRecord"
+                            name="patientMedicalRecord"
+                            placeholder="Enter your password" 
+                            value={patientMedicalRecord}
+                            onChange={(e) => onInputChange(e)}
+                            maxlength="200"
+                        /><br/> 
 
                         
-                        
-                                          
+                                                            
                         <button type="submit" value="Submit" id="submit">Submit</button>
                     </form>
                 </div>
