352cbfe55b49d7ecc6563ea8e1214ca8e58ceafb
bia3334 <quankun3357@gmail.com>
2023-04-17 21:13:01 +0700

    Add TakeCare class and AppointmentController

---
+++ Backend/HPDM/src/main/java/com/application/HPDM/TakeCare/TakeCareServiceImplementation.java
@@ -0,0 +1,36 @@
+package com.application.HPDM.TakeCare;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+@Service
+
+public class TakeCareServiceImplementation implements TakeCareService{
+    @Autowired
+    private TakeCareRespository takeCareRespository;
+    @Override
+    public TakeCare saveTakeCare(TakeCare takeCare) {
+        return takeCareRespository.save(takeCare);
+    }
+
+    @Override
+    public List<TakeCare> fetchTakeCareList() {
+        return takeCareRespository.findAll();
+    }
+
+    @Override
+    public TakeCare fetchTakeCareByPatientID(Long patientId) {
+        Optional<TakeCare> takeCare =
+                takeCareRespository.findById(patientId);
+        return takeCare.get();
+    }
+
+    @Override
+    public TakeCare fetchTakeCareByDoctorID(Long doctorId) {
+        Optional<TakeCare> takeCare =
+                takeCareRespository.findById(doctorId);
+        return takeCare.get();
+    }
+}
