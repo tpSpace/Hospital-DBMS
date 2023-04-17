4c0526d8fd0d737162865ba99b6dd07bd11071db
tpSpace <ITCSIU21081@student.hcmiu.edu.vn>
2023-04-17 23:06:17 +0700

    Resolve conflict

--- Backend/HPDM/src/main/java/com/application/HPDM/Doctor/DoctorController.java
+++ Backend/HPDM/src/main/java/com/application/HPDM/Doctor/DoctorController.java
@@ -1,6 +1,5 @@
 package com.application.HPDM.Doctor;
 
-import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
--- Backend/HPDM/src/main/java/com/application/HPDM/Login/LoginController.java
+++ Backend/HPDM/src/main/java/com/application/HPDM/Login/LoginController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin("*")
 public class LoginController {
 
-
     @Autowired
     private PatientRepository patientRepository;
 
