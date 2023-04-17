8c0b9c2eb9838e0e158dd0990560f1030b039484
Ho-Gia-Khang <hogiakhanglatoi@gmail.com>
2023-04-17 09:46:03 +0700

    added the axios dependency

--- Frontend/frontend-pdm/src/index.js
+++ Frontend/frontend-pdm/src/index.js
@@ -7,9 +7,9 @@ import {BrowserRouter} from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter> 
-      <App />
-    </BrowserRouter>
+      <BrowserRouter> 
+        <App />
+      </BrowserRouter>
   </React.StrictMode>
 );
 
