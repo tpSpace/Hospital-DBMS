9b9a8b6b2994a35dea120680f8f114129248a9bd
tpSpace <ITCSIU21081@student.hcmiu.edu.vn>
2023-04-17 11:13:10 +0700

    Fixed room having duplicated patients & delete staff contraints

--- Backend/HPDM/src/main/java/com/application/HPDM/Room/RoomConfig.java
+++ Backend/HPDM/src/main/java/com/application/HPDM/Room/RoomConfig.java
@@ -1,10 +1,12 @@
 package com.application.HPDM.Room;
 
 import com.application.HPDM.Patient.Patient;
+import com.application.HPDM.Patient.PatientController;
 import com.application.HPDM.Patient.PatientRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.time.LocalDate;
 import java.time.Month;
@@ -13,15 +15,16 @@ import java.util.List;
 @Configuration
 public class RoomConfig {
     @Bean
-    CommandLineRunner roomCommandLineRunner(RoomRepository roomRepository, PatientRepository patientRepository) {
+    CommandLineRunner roomCommandLineRunner(RoomRepository roomRepository, PatientController patientController) {
         return args -> {
-            Patient patient1 = new Patient("Khoi","Nguyen", LocalDate.of(1996, Month.JANUARY, 2),"Male","N/A","0903","something@who.com","schaupielerin");
-            Patient patient2 = new Patient("Tinle","Alex",LocalDate.of(2003,Month.APRIL,1),"Female","N/A","094353","hey@question.com","helwefweflo");
-            patientRepository.saveAll(List.of(patient1,patient2));
-
+//            Patient patient1 = new Patient("Khoi","Nguyen", LocalDate.of(1996, Month.JANUARY, 2),"Male","N/A","0903","something@who.com","schaupielerin");
+//            Patient patient2 = new Patient("Tinle","Alex",LocalDate.of(2003,Month.APRIL,1),"Female","N/A","094353","hey@question.com","helwefweflo");
+//            patientRepository.saveAll(List.of(patient1,patient2));
+            Patient patient1 = patientController.getPatientById(1L);
+            Patient patient2 = patientController.getPatientById(2L);
             Room room1 = new Room("roomName1", patient1);
             Room room2 = new Room("roomName2", patient2);
-
+            
             roomRepository.saveAll(List.of(room1, room2));
         };
     }
