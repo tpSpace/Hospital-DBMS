26fcdf321a7207ca51e85cada7fcaad994e5af5d
Ho-Gia-Khang <hogiakhanglatoi@gmail.com>
2023-04-17 12:45:58 +0700

    trying to make a login api at backend

--- Backend/HPDM/src/main/java/com/application/HPDM/Login/LoginController.java
+++ Backend/HPDM/src/main/java/com/application/HPDM/Login/LoginController.java
@@ -1,10 +1,23 @@
 package com.application.HPDM.Login;
 
+import com.application.HPDM.Doctor.Doctor;
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LoginController {
 
+//    @Autowired
+//    private AuthenticationManager authenticationManager;
+//    private JwtToken
+//    @PostMapping(path = "/doctor/login")
+//    public ResponseEntity<Doctor> doctorLogin(@RequestBody LoginRequest loginRequest){
+//        final Authentication authentication =
+//    }
 
 }
