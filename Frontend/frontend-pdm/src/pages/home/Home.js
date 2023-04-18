da1786c5cd8b86c4b3f85e694d770ec438d39341
Ho-Gia-Khang <hogiakhanglatoi@gmail.com>
2023-04-18 23:04:07 +0700

    trying to give the authority to other pages

--- Frontend/frontend-pdm/src/pages/home/Home.js
+++ Frontend/frontend-pdm/src/pages/home/Home.js
@@ -1,7 +1,15 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import { getAuthorize } from '../login/Login';
+
 const Home = () => {
+    const role = getAuthorize();
     const [appointments, setAppointments] = useState([]);
+    const [auth, setAuth] = useState(false);
+
+    if(role){
+        setAuth(true);
+    }
 
     useEffect(() => {
         loadAppointments();
@@ -10,38 +18,45 @@ const Home = () => {
     const loadAppointments = async() => {
         const result = await axios.get("http://localhost:8090/appointment");
         setAppointments(result.data);
-        console.log(result.data);
     }
 
     return (
-        <div className='container'>
-            <div className='py-4'>
-                <table className="table border shadow">
-                    <thead>
-                        <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">doctor</th>
-                            <th scope="col">patient</th>
-                            <th scope="col">date</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            appointments.map((appointment, index) => (
+        <>
+            {auth ? (
+                <div className='container'>
+                    <div className='py-4'>
+                        <table className="table border shadow">
+                            <thead>
                                 <tr>
-                                    <th scope="row" key={index}>{index + 1}</th>
-                                    <td>{appointment.doctorName}</td>
-                                    <td>{appointment.patientName}</td>
-                                    <td>{appointment.date}</td>
-                                    
+                                    <th scope="col">#</th>
+                                    <th scope="col">doctor</th>
+                                    <th scope="col">patient</th>
+                                    <th scope="col">date</th>
                                 </tr>
-                            ))
-                        }
-                    </tbody>
-                </table>
-            </div>
+                            </thead>
+                            <tbody>
+                                {
+                                    appointments.map((appointment, index) => (
+                                        <tr>
+                                            <th scope="row" key={index}>{index + 1}</th>
+                                            <td>{appointment.doctorName}</td>
+                                            <td>{appointment.patientName}</td>
+                                            <td>{appointment.date}</td>
+                                            
+                                        </tr>
+                                    ))
+                                }
+                            </tbody>
+                        </table>
+                    </div>
+                
+                </div>
             
-        </div>
+            ) : (
+                <div>Nothing to display</div>
+            )}
+            
+        </>
     );
 };
 // 
--- Frontend/frontend-pdm/src/pages/login/Login.js
+++ Frontend/frontend-pdm/src/pages/login/Login.js
@@ -4,6 +4,10 @@ import { useState, useRef, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
+export const getAuthorize = () => {
+    return localStorage.getItem('myRole');
+}
+
 const LoginDoctor = () => {
     let navigate = useNavigate();
     const userRef = useRef();
@@ -12,32 +16,46 @@ const LoginDoctor = () => {
     const [email, setEmail] = useState('');
     const [pwd, setPwd] = useState('');
     const [errMsg, setErrMsg] = useState('');
-    const [success, setSuccess] = useState(false);
+    const [role, setRole] = useState('');
+
     const [user,setUser] = useState({
         email: "",
         password: "",
-    })
+    });
+
+    // if(localStorage.getItem('myRole')){
+    //     localStorage.removeItem('myRole');
+    // }
+
+    useEffect(() => {
+        localStorage.setItem('myRole', role);
+        console.log(localStorage.getItem('myRole'));
+    }, [role]);
+
     useEffect(() => {
         userRef.current.focus();
     }, []);
 
     useEffect(() => {
         setErrMsg('');
-    },[email, pwd]);
+    },[email, pwd]);;
 
     const handleSubmit = async (e) =>{
         e.preventDefault();
         setUser({
             email: email,
             password: pwd,
-        })
+        });
+
         await axios.post("http://localhost:8090/login",user)
         .then(response => {
             console.log(response);
             if(response.status === 200){
                 setEmail('');
-                setPwd('')
-                setSuccess(true);
+                setPwd('');
+                setRole(JSON.stringify(response.data));
+                localStorage.setItem('myRole', role);
+                console.log('login successful');
                 navigate('/');
             }
         })
@@ -47,48 +65,40 @@ const LoginDoctor = () => {
                 setErrMsg('Wrong email or password');
             }
             console.log('login failed');
-        })
+        });
     }
+
     return (
-        <>
-            {success ? (
-                <section className='body-login'>
-                    <h1 className='title-login'>You are logged in!</h1>
-                </section>
-            ) : (
-                <section className='body-login'>
-                    <p ref={errRef} className={errMsg ? 'errmsg' : 'offscreen'} aria-live='assertive'>{errMsg}</p>
-                    <form className='container-login' onSubmit={handleSubmit}>
-                        <h1 className='title-login'>Sign In</h1>
-                        <br></br>
-                        <label htmlFor='username-login'>Username</label>
-                        <input
-                            type='text'
-                            id='username-login'
-                            placeholder='Enter your username'
-                            ref={userRef}
-                            autoComplete='off'
-                            onChange={(e) => setEmail(e.target.value)}
-                            value={email}
-                            required
-                        />
+        <section className='body-login'>
+            <p ref={errRef} className={errMsg ? 'errmsg' : 'offscreen'} aria-live='assertive'>{errMsg}</p>
+            <form className='container-login' onSubmit={handleSubmit}>
+                <h1 className='title-login'>Sign In</h1>
+                <br></br>
+                <label htmlFor='username-login'>Username</label>
+                <input
+                    type='text'
+                    id='username-login'
+                    placeholder='Enter your username'
+                    ref={userRef}
+                    autoComplete='off'
+                    onChange={(e) => setEmail(e.target.value)}
+                    value={email}
+                    required
+                />
 
-                        <label htmlFor='password-login'>Password</label>
-                        <input
-                            type='password'
-                            id='password-login'
-                            placeholder='Enter your password'
-                            onChange={(e) => setPwd(e.target.value)}
-                            value={pwd}
-                            required
-                        />
+                <label htmlFor='password-login'>Password</label>
+                <input
+                    type='password'
+                    id='password-login'
+                    placeholder='Enter your password'
+                    onChange={(e) => setPwd(e.target.value)}
+                    value={pwd}
+                    required
+                />
 
-                        <button type='submit' value='submit' className='submit-button-login'>Sign in</button>
-                    </form>
-                </section>
-            )}
-        </>
-    );
-};
+                <button type='submit' value='submit' className='submit-button-login'>Sign in</button>
+            </form>
+        </section>
+    )}
 
 export default LoginDoctor;
