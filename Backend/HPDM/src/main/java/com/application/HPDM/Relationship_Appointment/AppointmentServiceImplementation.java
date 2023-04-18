cdc969abb2857bfb988d6cd6d25e3237a00f81e7
Ho-Gia-Khang <hogiakhanglatoi@gmail.com>
2023-04-18 09:03:31 +0700

    trying to create a table that loads the appointment table from the database

--- Backend/HPDM/src/main/java/com/application/HPDM/Relationship_Appointment/AppointmentServiceImplementation.java
+++ Backend/HPDM/src/main/java/com/application/HPDM/Relationship_Appointment/AppointmentServiceImplementation.java
@@ -2,10 +2,16 @@ package com.application.HPDM.Relationship_Appointment;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Optional;
 
+@RestController
+@CrossOrigin("*")
 @Service
 public class AppointmentServiceImplementation implements AppointmentService{
     @Autowired
--- Frontend/frontend-pdm/src/pages/home/Home.js
+++ Frontend/frontend-pdm/src/pages/home/Home.js
@@ -1,9 +1,45 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 const Home = () => {
+    const [appointments, setAppointments] = useState([]);
+
+    useEffect(() => {
+        loadAppointments();
+    }, []);
+
+    const loadAppointments = async() => {
+        const result = await axios.get("http://localhost:8080/appointments");
+        setAppointments(result);
+    };
+
     return (
-        <div>
-            <h1>Home</h1>
+        <div className='container-home'>
+            <div className='py-4'>
+                <table className="table border shadow">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">doctor</th>
+                            <th scope="col">patient</th>
+                            <th scope="col">date</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            appointments.map((appointment, index) => (
+                                <tr>
+                                    <th scope="row" key={index}>{index + 1}</th>
+                                    <td>{appointment.doctocName}</td>
+                                    <td>{appointment.patientName}</td>
+                                    <td>{appointment.Date}</td>
+                                    
+                                </tr>
+                            ))
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
