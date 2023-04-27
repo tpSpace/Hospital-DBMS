06d14bd6ea7468125accdf812471c884cd3aeb47
tpSpace <ITCSIU21081@student.hcmiu.edu.vn>
2023-04-28 00:55:07 +0700

    Refactor code && re-organise files and functions

--- Frontend/frontend-pdm/src/pages/home/Home.js
+++ Frontend/frontend-pdm/src/pages/home/Home.js
@@ -1,44 +1,11 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 const Home = () => {
-    const [appointments, setAppointments] = useState([]);
-
-    useEffect(() => {
-        loadAppointments();
-    }, []);
-
-    const loadAppointments = async() => {
-        const result = await axios.get("http://localhost:8090/appointment");
-        setAppointments(result.data);
-    }
+    
 
     return (
         <div className='container-home'>
-            <div className='py-4'>
-                <table className="table border shadow">
-                    <thead>
-                        <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">doctor</th>
-                            <th scope="col">patient</th>
-                            <th scope="col">date</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            appointments.map((appointment, index) => (
-                                <tr key={index}>
-                                    <th scope="row" >{index + 1}</th>
-                                    <td>{appointment.doctorName}</td>
-                                    <td>{appointment.patientName}</td>
-                                    <td>{appointment.date}</td>
-                                </tr>
-                            ))
-                        }
-                    </tbody>
-                </table>
-            </div>
-            
+            <p>Hi this is our landing page?</p>
         </div>
     );
 };
