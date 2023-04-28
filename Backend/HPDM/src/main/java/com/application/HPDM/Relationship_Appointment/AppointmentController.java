4b4bc8b7e656f81d54e48e0395b28d4e2503c3bf
tpSpace <ITCSIU21081@student.hcmiu.edu.vn>
2023-04-29 01:22:04 +0700

    Return message is JSON & return id

--- Backend/HPDM/src/main/java/com/application/HPDM/Relationship_Appointment/AppointmentController.java
+++ Backend/HPDM/src/main/java/com/application/HPDM/Relationship_Appointment/AppointmentController.java
@@ -1,14 +1,10 @@
 package com.application.HPDM.Relationship_Appointment;
 
-import com.application.HPDM.Doctor.Doctor;
-import com.application.HPDM.Patient.Patient;
-import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDate;
 import java.util.List;
 
 @RestController
