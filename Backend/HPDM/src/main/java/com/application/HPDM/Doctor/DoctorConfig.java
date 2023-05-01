7e026a99d7f03c02ca4aba5639d23d4e2e9d948e
khang <khangpg902@gmail.com>
2023-05-01 11:59:04 +0700

    added data for doctor,nurse, staff

--- Backend/HPDM/src/main/java/com/application/HPDM/Doctor/DoctorConfig.java
+++ Backend/HPDM/src/main/java/com/application/HPDM/Doctor/DoctorConfig.java
@@ -16,15 +16,70 @@ public class DoctorConfig {
     CommandLineRunner doctorCommandLineRunner(DoctorRepository doctorRepository,
                                               DepartmentController departmentController){
         return args -> {
-            Department ngu = departmentController.getDepartmentById(1L);
-            Department vl = departmentController.getDepartmentById(2L);
+
             Doctor Alex = new Doctor("Alex","0903696969",
-                    LocalDate.of(1996, Month.JANUARY, 2), ngu , "alex@gmail.com", "alex123");
-            Doctor amongus = new Doctor("sus","42096969",
-                    LocalDate.of(26, Month.JANUARY, 1), ngu, "amongus69@gmail.com", "AMONGUS" );
-            Doctor vaidan = new Doctor("vd","445969",
-                    LocalDate.of(2604, Month.JANUARY, 1), vl , "kkmanman@gmail.com", "choll123");
-            doctorRepository.saveAll(List.of(Alex,amongus,vaidan));
+                    LocalDate.of(1996, Month.JANUARY, 2), departmentController.getDepartmentById(1L),
+                    "alex@gmail.com", "alex123");
+            Doctor Among = new Doctor("Among Us","0937276192",
+                    LocalDate.of(1969, Month.JUNE, 9), departmentController.getDepartmentById(2L),
+                    "amongus69@gmail.com", "AMONGUS" );
+            Doctor Khai = new Doctor("Khai Ng.","4459694223",
+                    LocalDate.of(1990, Month.JANUARY, 1), departmentController.getDepartmentById(2L) ,
+                    "khaiNg@gmail.com", "123456");
+            Doctor Brian = new Doctor("Brian Yip","7749142",
+                    LocalDate.of(1985, Month.FEBRUARY, 14), departmentController.getDepartmentById(3L) ,
+                    "Bryan@gmail.com", "HiIMBr");
+            Doctor Trayarus = new Doctor("Dr.Trayarus","09564201",
+                    LocalDate.of(1960, Month.FEBRUARY, 14), departmentController.getDepartmentById(5L) ,
+                    "Trayarus@gmail.com", "BurnThem");
+            Doctor Yippy = new Doctor("Happy Yippy","555777666",
+                    LocalDate.of(2000, Month.MARCH, 30), departmentController.getDepartmentById(4L) ,
+                    "HappyHappy@gmail.com", "HappyHappyHappy");
+            Doctor KaKa = new Doctor("KaManMan","555555555",
+                    LocalDate.of(1955, Month.MAY, 5), departmentController.getDepartmentById(6L) ,
+                    "kkmanman@gmail.com", "gk3zck");
+            Doctor Sery = new Doctor("Seryph Acker","022325839",
+                    LocalDate.of(2001, Month.JULY, 27), departmentController.getDepartmentById(10L) ,
+                    "SeryphAcker@gmail.com", "HIHELLLOWHY");
+            Doctor MunD = new Doctor("Mund Mundo","000010232",
+                    LocalDate.of(1990, Month.MARCH,9), departmentController.getDepartmentById(7L) ,
+                    "Mundogoeswhere@gmail.com", "hePleases");
+            Doctor Neil = new Doctor("Neil Grapes","870987521",
+                    LocalDate.of(1990, Month.APRIL, 30), departmentController.getDepartmentById(8L) ,
+                    "ilikegrapes@gmail.com", "Neil.G");
+            Doctor Linus = new Doctor("Linus Xeno","77985926",
+                    LocalDate.of(1991, Month.DECEMBER, 03), departmentController.getDepartmentById(3L) ,
+                    "LinuxTechTip@gmail.com", "HiIMBr");
+            Doctor Phuc = new Doctor("Phuc Ng.","242293369",
+                    LocalDate.of(1990, Month.MARCH, 8), departmentController.getDepartmentById(4L) ,
+                    "phucnguyendu@gmail.com", "23");
+            Doctor Chisato = new Doctor("Chisa Chisato","2421271",
+                    LocalDate.of(1990, Month.AUGUST, 07), departmentController.getDepartmentById(4L) ,
+                    "ChisatoC@gmail.com", "Takima<3");
+            Doctor Blue = new Doctor("Blue Rain","4454332169",
+                    LocalDate.of(1987, Month.APRIL, 04), departmentController.getDepartmentById(5L) ,
+                    "bluer@gmail.com", "09588111");
+            Doctor cheese = new Doctor("Cheezy Weeazy","454230011",
+                    LocalDate.of(1956, Month.JUNE , 14), departmentController.getDepartmentById(6L) ,
+                    "Cheese@gmail.com", "squeeksqueek");
+            Doctor Axees = new Doctor("Traxy Che","3241249",
+                    LocalDate.of(1990, Month.FEBRUARY, 14), departmentController.getDepartmentById(7L) ,
+                    "TaxyC@gmail.com", "I122T");
+            Doctor Cuu = new Doctor("Heloc Blindins","293190421",
+                    LocalDate.of(1978, Month.SEPTEMBER, 8), departmentController.getDepartmentById(8L) ,
+                    "Helloc@gmail.com", "0606060203");
+            Doctor Hetcuu = new Doctor("Recale Poker","231423122",
+                    LocalDate.of(1911, Month.OCTOBER, 21), departmentController.getDepartmentById(9L) ,
+                    "RecaleP@gmail.com", "HelpmeOMG");
+            Doctor Plane = new Doctor("Planer Lainer","090909099",
+                    LocalDate.of(2001, Month.SEPTEMBER, 11), departmentController.getDepartmentById(10L) ,
+                    "Plane@gmail.com", "ilovebuildings");
+            Doctor Spidery = new Doctor("Muffet Pupper","1242199999",
+                    LocalDate.of(1982, Month.SEPTEMBER, 1), departmentController.getDepartmentById(4L) ,
+                    "Muffeter@gmail.com", "donut123");
+            doctorRepository.saveAll(List.of(Alex,Among,Brian,Khai,Linus,
+                    MunD,Spidery,Hetcuu,Cuu,Plane,cheese,Axees,Phuc,Chisato,
+                    Neil,Blue,KaKa,Sery,Yippy,Trayarus));
         };
     }
 }
--- Backend/HPDM/src/main/java/com/application/HPDM/Staff/StaffConfig.java
+++ Backend/HPDM/src/main/java/com/application/HPDM/Staff/StaffConfig.java
@@ -16,11 +16,43 @@ public class StaffConfig {
     @Bean
     CommandLineRunner staffCommandLineRunner(StaffRepository staffRepository, DepartmentController departmentController) {
         return  args -> {
-            Department department1 = departmentController.getDepartmentById(1L);
-            Department department2 = departmentController.getDepartmentById(2L);
-            Staff staff1 = new Staff("Hung", LocalDate.of(2003, Month.DECEMBER,23),department1,"0902934234","whatname@nick.com","nopassword");
-            Staff staff2 = new Staff("Khoi", LocalDate.of(2003, Month.DECEMBER,23),department2,"43354523424","nolife@sick.com","nopassword");
-            staffRepository.saveAll(List.of(staff1, staff2));
+            Staff a = new Staff("Hung Nguyen", LocalDate.of(2003, Month.DECEMBER,23),
+                    departmentController.getDepartmentById(1L),
+                    "0902934234","whatname@nick.com","nopassword1");
+            Staff b = new Staff("Khoi Ng.", LocalDate.of(2003, Month.MARCH,8),
+                    departmentController.getDepartmentById(2L),
+                    "43354523424","nolife@sick.com","nopassword2");
+            Staff c = new Staff("Huy Ngo", LocalDate.of(2003, Month.JUNE,29),
+                    departmentController.getDepartmentById(2L),
+                    "0946274888","perterhuy@gmail.com","nopassword3");
+            Staff d = new Staff("Khang Ho", LocalDate.of(2003, Month.DECEMBER,25),
+                    departmentController.getDepartmentById(3L),
+                    "43232114","khangdethuong@gmail.com","kkmanman");
+            Staff e = new Staff("Khang Fam", LocalDate.of(2003, Month.FEBRUARY,9),
+                    departmentController.getDepartmentById(3L),
+                    "0946180277","khangbdqua@gmail.com","kkmanman2");
+            Staff f = new Staff("Binh Nguyen", LocalDate.of(2003, Month.DECEMBER,19),
+                    departmentController.getDepartmentById(4L),
+                    "093433314","mrbin@gmail.com","kk231a231n");
+            Staff g = new Staff("Ton ND", LocalDate.of(2003, Month.MARCH,31),
+                    departmentController.getDepartmentById(5L),
+                    "093553873","tonnd@gmail.com","kkmanman3123");
+            Staff h = new Staff("Tohsaka Rin", LocalDate.of(1980, Month.FEBRUARY,3),
+                    departmentController.getDepartmentById(6L),
+                    "091538366","tohsakarin0302@gmail.com","duchuybdvai");
+            Staff i = new Staff("Meowwo", LocalDate.of(1999, Month.MAY ,29),
+                    departmentController.getDepartmentById(7L),
+                    "090932114","meowowowo@gmail.com","imverybadowo");
+            Staff j = new Staff("Auby Rey", LocalDate.of(2001, Month.JANUARY,12),
+                    departmentController.getDepartmentById(8L),
+                    "0843121214","aubryadwwd@gmail.com","vaicadanluon");
+            Staff k = new Staff("Mari Omi", LocalDate.of(1998, Month.MARCH,1),
+                    departmentController.getDepartmentById(9L),
+                    "091232333","iloveyousunny@gmail.com","something!");
+            Staff l = new Staff("Basil Leaft", LocalDate.of(2000, Month.FEBRUARY,18),
+                    departmentController.getDepartmentById(10L),
+                    "091326667","flowerboy@gmail.com","sunflowers");
+            staffRepository.saveAll(List.of(a, b,c,d,e,f,g,h,i,j,k,l));
 
         };
     }
